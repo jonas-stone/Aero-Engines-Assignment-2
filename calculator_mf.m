c94ca48b28b8c8fc65b1c773847d0856bf42b762
matteoalradice <matteo.al.radice@gmail.com>
2025-12-23 17:36:28 +0100

    g

--- calculator_mf.m
+++ calculator_mf.m
@@ -1,31 +1,47 @@
 clear all; close all; clc;
 
-thrust = [25; 50; 100; 200; 300;]; % 25 is idle
-T3 = [480; 580; 660; 760; 830;];
-P3 = [650; 1000; 1750; 3000; 4300;];
-T4 = [960; 1050; 1225; 1450; 1650;];
-m_a = [20; 29; 47; 76; 98;];
+%        idle  50    100   200   300   
+thrust = [25;  50;   100;  200;  300];  % [kN]
+T3     = [480; 580;  660;  760;  830];  % [K]
+P3     = [650; 1000; 1750; 3000; 4300]; % [Pa]
+T4     = [960; 1050; 1225; 1450; 1650]; % [K]
+m_a    = [20;  29;   47;   76;   98];   % [kg/s]
 
 % --- Constants ---
-Cp_air = 1000;       
-Cp_gas = 1150;     
-LHV = 43.6 * 10^6;  
+Cp_air = 1000;      % [J/kg K]  
+Cp_gas = 1150;      % [J/kg K]
+LHV = 43.6 * 10^6;  % [J/kg]
 
 % mf = ma * (Cpg*T4 - Cpa*T3) / (LHV - Cpg*T4)
+
+% % % % % % % % % % % % % % Fuel massflow % % % % % % % % % % % % % % % % %
+
 m_f = m_a .* (Cp_gas .* T4 - Cp_air .* T3) ./ (LHV - (Cp_gas .* T4));
 
+% % % % % % % % % % % % Equivalence Ratio % % % % % % % % % % % % % % % % %
+% Air is modelled as a mixture of ideal gases:
+% -> 79% in volume is N2
+% -> 21% in volume is O2
 
+% Air mass per a single fuel mole 
+moles_O2 = 16.75;
+moles_N2 = 16.75 * 3.7619;
+mass_air = (moles_O2 * 32) + (moles_N2 * (14.01 * 2));
 
-mass_fuel_stoich = (11 * 12) + (23 * 1.008); 
-moles_O2_stoich = 16.75;
-moles_N2_stoich = 16.75 * 3.76;
-mass_air_stoich = (moles_O2_stoich * 32) + (moles_N2_stoich * (14.01 * 2));
+% Single fuel mole mass
+mass_fuel = (11 * 12) + (23 * 1.008); 
 
-FAR_stoich = mass_fuel_stoich / mass_air_stoich;
+% Stechiometric Fuel-to-Air ratio
+FAR_ref = mass_fuel / mass_air;
 
+% Actual Fuel-to-Air ratio
 FAR_actual = m_f ./ m_a; 
 
-phi = FAR_actual / FAR_stoich;
+% Equivalence ratio
+phi = FAR_actual ./ FAR_ref;
+
+% NOTE: following the proposed definition Phi > 1 is fuel rich mixture, Phi < 1 is
+% oxydizer rich mixture
 
 % --- Results ---
 fprintf('\nThrust(kN)\tAirFlow(kg/s)\tFuelFlow(kg/s)\tFAR_actual\tPhi\n');
