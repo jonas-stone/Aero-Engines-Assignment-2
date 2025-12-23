5e136159c59d87de0cdd196f3959c2dcd599283a
jonas <jonasstonealv@gmail.com>
2025-12-23 15:44:58 +0100

    x

--- calculator_mf.m
+++ calculator_mf.m
@@ -12,6 +12,24 @@ LHV = 43.6 * 10^6;
 % mf = ma * (Cpg*T4 - Cpa*T3) / (LHV - Cpg*T4)
 m_f = m_a .* (Cp_gas .* T4 - Cp_air .* T3) ./ (LHV - (Cp_gas .* T4));
 
+
+
+mass_fuel_stoich = (11 * 12) + (23 * 1.008); 
+moles_O2_stoich = 16.75;
+moles_N2_stoich = 16.75 * 3.76;
+mass_air_stoich = (moles_O2_stoich * 32) + (moles_N2_stoich * (14.01 * 2));
+
+FAR_stoich = mass_fuel_stoich / mass_air_stoich;
+
+FAR_actual = m_f ./ m_a; 
+
+phi = FAR_actual / FAR_stoich;
+
 % --- Results ---
-disp('   Thrust(kN)   AirFlow(kg/s)   FuelFlow(kg/s)');
-disp([thrust, m_a, m_f]);
+fprintf('\nThrust(kN)\tAirFlow(kg/s)\tFuelFlow(kg/s)\tFAR_actual\tPhi\n');
+fprintf('--------------------------------------------------------------------------\n');
+
+for i = 1:length(thrust)
+    fprintf('%.0f\t\t%.2f\t\t%.4f\t\t%.5f\t\t%.4f\n', ...
+        thrust(i), m_a(i), m_f(i), FAR_actual(i), phi(i));
+end
